5d8f45ee975f21a678fd75c9b504d2b6fdddaf59
Miller-Vidal <211102@ids.upchiapas.edu.mx>
2023-07-28 10:13:09 -0600

    Recuadros agregados

--- src/DocumentView.js
+++ src/DocumentView.js
@@ -1,21 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './document.css';
 
 axios.defaults.withCredentials = true;
 
 const DocumentView = () => {
-  const [command, setCommand] = useState('');
+  const [command1, setCommand1] = useState('');
+  const [command2, setCommand2] = useState('');
+  const [command3, setCommand3] = useState('');
   const [output, setOutput] = useState('');
   const [lexico, setLexico] = useState('');
   const [tokens, setTokens] = useState([]);
   const [tokenInfo, setTokenInfo] = useState([]);
   const [reservedInfo, setReservedInfo] = useState([]);
+  const [isCommand1Executed, setIsCommand1Executed] = useState(false);
+  const [isCommand2Executed, setIsCommand2Executed] = useState(false);
 
   // Nuevo estado para visualizar el código consumido
   const [inputCode, setInputCode] = useState('');
 
-  const handleCommand = (e) => {
+  useEffect(() => {
+    if (isCommand1Executed) {
+      // Ejecutar el "Command 2" si "Command 1" ha sido ejecutado con éxito
+      axios.post('http://localhost:5000/command', { command: command2 })
+        .then((response) => {
+          if (response.data.message === "command executed") {
+            console.log(response.data.output);
+            setOutput(response.data.output);
+            // Marcar el "Command 2" como ejecutado
+            setIsCommand2Executed(true);
+          } else {
+            console.log('Error al enviar el Command 2:', response.data.message);
+          }
+        })
+        .catch((error) => {
+          console.log('Error al enviar el Command 2:', error);
+        });
+    }
+  }, [isCommand1Executed, command2]);
+
+  useEffect(() => {
+    if (isCommand2Executed) {
+      // Ejecutar el "Command 3" si "Command 2" ha sido ejecutado con éxito
+      axios.post('http://localhost:5000/command', { command: command3 })
+        .then((response) => {
+          if (response.data.message === "command executed") {
+            console.log(response.data.output);
+            setOutput(response.data.output);
+          } else {
+            console.log('Error al enviar el Command 3:', response.data.message);
+          }
+        })
+        .catch((error) => {
+          console.log('Error al enviar el Command 3:', error);
+        });
+    }
+  }, [isCommand2Executed, command3]);
+
+  const handleCommand = (e, command) => {
     e.preventDefault();
     // Actualizar el estado del código consumido
     setInputCode(command);
@@ -25,32 +67,35 @@ const DocumentView = () => {
         if (response.data.message === "command executed") {
           console.log(response.data.output);
           setOutput(response.data.output);
+          // Marcar el "Command 1" como ejecutado
+          setIsCommand1Executed(true);
         } else {
-          console.log('Error al enviar el comando:', response.data.message);
+          console.log('Error al enviar el Command 1:', response.data.message);
         }
       })
       .catch((error) => {
-        console.log('Error al enviar el comando:', error);
+        console.log('Error al enviar el Command 1:', error);
       });
-      
-    axios.post('http://localhost:5000/parse_code', { code: command })
-      .then((response) => {
-        console.log(response.data.result);
-        setLexico(response.data.result);
-        setTokens(JSON.stringify(response.data.tokens, null, 2));
-        setReservedInfo(JSON.stringify(response.data.reserved, null, 2));
-      })
-      .catch((error) => {
-        console.log('Error al enviar el comando:', error);
-      });
+  };
 
-    axios.post('http://localhost:5000/token_info', { code: command })
+  const handleCommand1 = (e) => {
+    e.preventDefault();
+    // Actualizar el estado del código consumido
+    setInputCode(command1);
+
+    axios.post('http://localhost:5000/command', { command: command1 })
       .then((response) => {
-        console.log(response.data);
-        setTokenInfo(JSON.stringify(response.data.tokens, null, 2));
+        if (response.data.message === "command executed") {
+          console.log(response.data.output);
+          setOutput(response.data.output);
+          // Marcar el "Command 1" como ejecutado
+          setIsCommand1Executed(true);
+        } else {
+          console.log('Error al enviar el Command 1:', response.data.message);
+        }
       })
       .catch((error) => {
-        console.log('Error al obtener la información de los tokens:', error);
+        console.log('Error al enviar el Command 1:', error);
       });
   };
 
@@ -59,21 +104,62 @@ const DocumentView = () => {
       <div className="container-fluid bg-color2">
         <div className="row">
           <div className="col-12 col-lg-6 mt-5">
-            <form onSubmit={handleCommand}>
-              <h4 className="sub-title">Command</h4>
-              <input 
-                type="text" 
-                className="sub-input" 
-                onChange={(e) => setCommand(e.target.value)} 
-                value={command}
+            <form onSubmit={handleCommand1}>
+              <h4 className="sub-title">Command 1</h4>
+              <input
+                type="text"
+                className="sub-input"
+                onChange={(e) => setCommand1(e.target.value)}
+                value={command1}
               />
               <button type="submit" className="btn btn-primary mt-3">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid bg-color2">
+        <div className="row">
+          <div className="col-12 col-lg-6 mt-5">
+            <form onSubmit={(e) => handleCommand(e, command2)} disabled={!isCommand1Executed}>
+              <h4 className="sub-title">Command 2</h4>
+              <input
+                type="text"
+                className="sub-input"
+                onChange={(e) => setCommand2(e.target.value)}
+                value={command2}
+                disabled={!isCommand1Executed}
+              />
+              <button type="submit" className="btn btn-primary mt-3" disabled={!isCommand1Executed}>
+                Send
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid bg-color2">
+        <div className="row">
+          <div className="col-12 col-lg-6 mt-5">
+            <form onSubmit={(e) => handleCommand(e, command3)} disabled={!isCommand2Executed}>
+              <h4 className="sub-title">Command 3</h4>
+              <input
+                type="text"
+                className="sub-input"
+                onChange={(e) => setCommand3(e.target.value)}
+                value={command3}
+                disabled={!isCommand2Executed}
+              />
+              <button type="submit" className="btn btn-primary mt-3" disabled={!isCommand2Executed}>
+                Send
+              </button>
             </form>
             <h4 className="sub-title mt-5">Output</h4>
             <textarea readOnly className="sub-input" value={output} />
           </div>
         </div>
       </div>
+
       <div className="container-fluid bg-color2">
         <div className="row">
           <div className="col-12 col-lg-6 mt-5">
@@ -88,6 +174,7 @@ const DocumentView = () => {
           </div>
         </div>
       </div>
+
       <div className="container-fluid bg-color2">
         <div className="row">
           <div className="col-12 col-lg-6 mt-5">
